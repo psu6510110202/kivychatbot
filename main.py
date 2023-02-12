5de9e2a91fed39997ab6c30f144c01af04a06790
ธรากร ชลายนคุปต์ <meowkhangban@gmail.com>
2023-02-12 16:20:17 +0700

    Adjust send text field

--- main.py
+++ main.py
@@ -32,9 +32,28 @@ class ChatBot(MDApp):
         return screen_manager
 
     def send(self):
+        global size, halign, value
         if screen_manager.get_screen('chats').text_input != "":
             value =  screen_manager.get_screen('chats').text_input.text
-            screen_manager.get_screen('chats').chat_list.add_widget(Command(text=value, size_hint_x=.2, halign="center", font_name="Poppins",))
+            if len(value) < 6:
+                size = .22
+                halign = "center"
+            elif len(value) < 11:
+                size = .32
+                halign = "center"
+            elif len(value) < 16:
+                size = .45
+                halign = "center"
+            elif len(value) < 21:
+                size = .58
+                halign = "center"
+            elif len(value) < 16:
+                size = .71
+                halign = "center"
+            else:
+                size = .77
+                halign = "left"
+            screen_manager.get_screen('chats').chat_list.add_widget(Command(text=value,size_hint_x = size, halign=halign, font_name="Poppins",))
 
 
 if __name__ == '__main__':
