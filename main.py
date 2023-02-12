e6280b36efbbe8f4ed9889ed8567521b4ec6bec1
ธรากร ชลายนคุปต์ <meowkhangban@gmail.com>
2023-02-12 16:15:27 +0700

    Add send message logic

--- main.py
+++ main.py
@@ -3,8 +3,21 @@ from kivy.lang import Builder
 from kivy.core.window  import Window
 from kivy.uix.screenmanager import ScreenManager
 from kivy.core.text import LabelBase
+from kivy.properties import StringProperty, NumericProperty
+from kivymd.uix.label import MDLabel
 
 Window.size = (350, 550)
+
+class Command(MDLabel):
+    text = StringProperty()
+    size_hint_x = NumericProperty()
+    halign = StringProperty()
+    font_name = "Poppins"
+    font_size = 17    
+    pass
+
+class Response(MDLabel):
+    pass
 
 class ChatBot(MDApp):
 
@@ -17,6 +30,12 @@ class ChatBot(MDApp):
         screen_manager.add_widget(Builder.load_file('Main.kv'))
         screen_manager.add_widget(Builder.load_file('Chats.kv'))
         return screen_manager
+
+    def send(self):
+        if screen_manager.get_screen('chats').text_input != "":
+            value =  screen_manager.get_screen('chats').text_input.text
+            screen_manager.get_screen('chats').chat_list.add_widget(Command(text=value, size_hint_x=.2, halign="center", font_name="Poppins",))
+
 
 if __name__ == '__main__':
     LabelBase.register(name="Poppins", fn_regular="Poppins-Regular.ttf")
